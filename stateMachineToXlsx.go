318d57167ea99ae69f4e239e011a19892b7507de
imtnd <tsunoda0330@gmail.com>
2019-03-08 23:10:07 +0900

    make sample excel file

--- stateMachineToXlsx.go
+++ stateMachineToXlsx.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/tealeg/xlsx"
+	"github.com/tealeg/xlsx"
 )
 
 func makeXlsx(sm StateMachine) {
 	fmt.Println(sm)
+
+	var file *xlsx.File
+	var sheet *xlsx.Sheet
+	var row *xlsx.Row
+	var cell *xlsx.Cell
+	var err error
+
+	file = xlsx.NewFile()
+	sheet, err = file.AddSheet("Sheet1")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	row = sheet.AddRow()
+	cell = row.AddCell()
+	cell.Value = "test"
+	err = file.Save("./sample.xlsx")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
 }
