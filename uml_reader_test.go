1edeb832f7609c973d7243a5f9ad0d2f1ea54826
imtnd <tsunoda0330@gmail.com>
2019-03-16 21:58:33 +0900

    add test

--- uml_reader_test.go
+++ uml_reader_test.go
@@ -3,32 +3,46 @@ package main
 import "testing"
 
 func Test_analyzeLine(t *testing.T) {
+	type fields struct {
+		States []string
+		Events []string
+	}
 	type args struct {
 		s string
 	}
 	tests := []struct {
 		name  string
 		args  args
-		wants []string
+		wants fields
 	}{
 		// TODO: Add test cases.
 		{
 			name:  "analyzeLine analyze state",
 			args:  args{"[*] -> second"},
-			wants: []string{"Start", "second"},
+			wants: fields{States: []string{"Start", "second"}, Events: nil},
 		},
 		{
 			name:  "analyzeLine analyze header",
 			args:  args{"@startuml{test.png}"},
-			wants: nil,
+			wants: fields{States: []string{"Start", "second"}, Events: nil},
+		},
+		{
+			name:  "analyzeLine analyze state and event",
+			args:  args{"second -> [*] : event1"},
+			wants: fields{States: []string{"Start", "second"}, Events: []string{"event1"}},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			analyzeLine(tt.args.s)
-			for _, tw := range tt.wants {
-				if !isContain(stateMachineInfo.States, tw) {
-					t.Errorf("analyzeLine() result %v, want %v", stateMachineInfo.States, tw)
+			for _, ts := range tt.wants.States {
+				if !isContain(stateMachineInfo.States, ts) {
+					t.Errorf("analyzeLine() result %v, want %v", stateMachineInfo.States, ts)
+				}
+			}
+			for _, te := range tt.wants.Events {
+				if !isContain(stateMachineInfo.Events, te) {
+					t.Errorf("analyzeLine() result %v, want %v", stateMachineInfo.Events, te)
 				}
 			}
 		})
